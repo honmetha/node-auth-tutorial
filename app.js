6fa3ba6808f754c8cbc386a6b426db246c6202ff
Hon <dennis.mejen@gmail.com>
2021-09-02 02:25:15 +0700

    [Added] 2 - Auth Routes & Controllers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -24,3 +25,4 @@ mongoose
 // routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
