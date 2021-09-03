bd72d64b4793e35a642a45c66ef3e5750f3b6c57
Hon <dennis.mejen@gmail.com>
2021-09-03 23:41:27 +0700

    [Added] 3 - Testing Routes & Handling POST Requests

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,9 +7,15 @@ module.exports.login_get = (req, res) => {
 };
 
 module.exports.signup_post = (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
   res.send("new signup");
 };
 
 module.exports.login_post = (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
   res.send("user login");
 };
