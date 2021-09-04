d943fa8de1422dda2035edde0552034bbf77f940
Hon <dennis.mejen@gmail.com>
2021-09-04 17:04:18 +0700

    [Added] 5 - Mongoose Validation (Part 2)

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,22 @@ const User = require("../models/User");
 // handle errors
 const handleErrors = (err) => {
   console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  // duplicate error code
+  if (err.code === 11000) {
+    errors.email = "that email is already registered";
+    return errors;
+  }
+
+  //validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
 };
 
 module.exports.signup_get = (req, res) => {
@@ -20,8 +36,8 @@ module.exports.signup_post = async (req, res) => {
     const user = await User.create({ email, password });
     res.status(200).json(user);
   } catch (err) {
-    handleErrors(err);
-    res.status(400).send("error, user not created");
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
 
