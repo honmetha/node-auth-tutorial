2f556b1dcba62cc0b2cfc7cf2ef4d6cc20a16113
Hon <dennis.mejen@gmail.com>
2021-09-15 00:04:38 +0700

    [Added] 11 - New User Signup (part 1)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 // handle errors
 const handleErrors = (err) => {
@@ -21,6 +22,13 @@ const handleErrors = (err) => {
   return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "victoria secret", {
+    expiresIn: maxAge,
+  });
+};
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -34,7 +42,9 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(200).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(200).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
