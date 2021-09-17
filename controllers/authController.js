fbc682f9f2342acad8c7575a268394a1f1175f7b
Hon <dennis.mejen@gmail.com>
2021-09-17 23:53:52 +0700

    [Added] 14 - Logging Users in (part 2)

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,16 @@ const handleErrors = (err) => {
   console.log(err.message, err.code);
   let errors = { email: "", password: "" };
 
+  // incorrect email
+  if (err.message === "incorrect email") {
+    errors.email = "that email is not registered";
+  }
+
+  // incorrect password
+  if (err.message === "incorrect password") {
+    errors.password = "that password is incorrect";
+  }
+
   // duplicate error code
   if (err.code === 11000) {
     errors.email = "that email is already registered";
@@ -56,8 +66,11 @@ module.exports.login_post = async (req, res) => {
 
   try {
     const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(200).json({ user: user._id });
   } catch (err) {
-    res.status(400).json({});
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
