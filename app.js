ed9ee328a7976c943a273af1831fbad29589e251
Hon <dennis.mejen@gmail.com>
2022-09-15 23:44:05 +0700

    🗑️ delete: template strings

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set("view engine", "ejs");
 
 // database connection
 mongoose
-  .connect(`${process.env.MONGO_URI}`, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
